f02316854eddf7883140ebaaa82f50554629ece0
Elham-AT <110053378+Elham-AT@users.noreply.github.com>
2022-12-12 15:08:55 -0500

    StringFormatter class added to reverse every other word in a sentence

--- string-manipulation/src/main/java/org/example/AlternatingLetters.java
+++ string-manipulation/src/main/java/org/example/AlternatingLetters.java
@@ -2,7 +2,12 @@ package org.example;
 
 public class AlternatingLetters {
     public static void main(String[] args) {
-        String s = "Spongebob";
+        String s1 = "Spongebob";
+        String s2 = "spongebob";
+
+        System.out.println(alternate(s1));
+        System.out.println(alternate(s2));
+
 
 
     }
@@ -11,6 +16,18 @@ public class AlternatingLetters {
     // ex: input -> Spongebob, output -> SpOnGeBoB
     // ex: input -> spongebob, output -> sPoNgEbOb
     public static String alternate(String s) {
-        return "";
+        char[] chars = s.toCharArray();
+        if(Character.isUpperCase(chars[0])){
+            for (int i = 0; i < s.length(); i+=2) {
+                chars[i] = Character.toUpperCase(chars[i]);
+            }
+        }else{
+            for (int i = 1; i < s.length(); i+=2) {
+                chars[i] = Character.toUpperCase(chars[i]);
+            }
+        }
+
+        String result = new String(chars);
+        return result;
     }
 }
